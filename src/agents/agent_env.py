bb5b7e6ae421392fe61455a5a37598ac76de7d10
nanli <linanenv@gmail.com>
2023-12-14 21:05:16 +0800

    update

---
+++ src/agents/agent_env.py
@@ -0,0 +1,56 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_to_openai_function_messages
+from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.tools.render import format_tool_to_openai_function
+
+load_dotenv()
+
+
+def env_agent():
+    tools = [DuckDuckGoSearchRun()]
+
+    chat_model = ChatOpenAI(
+        model=os.getenv("OPENAI_MODEL"),
+        temperature=0,
+        verbose=True,
+    )
+
+    MEMORY_KEY = "chat_history"
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "Your role is to act as a representative from China's Ministry of Ecology and Environment, expressing deep concern about the improper disposal of waste batteries leading to environmental pollution and ecological risks.",
+            ),
+            MessagesPlaceholder(variable_name=MEMORY_KEY),
+            ("user", "{input}"),
+            MessagesPlaceholder(variable_name="agent_scratchpad"),
+        ]
+    )
+
+    chat_model_with_tools = chat_model.bind(
+        functions=[format_tool_to_openai_function(t) for t in tools]
+    )
+
+    agent = (
+        {
+            "input": lambda x: x["input"],
+            "agent_scratchpad": lambda x: format_to_openai_function_messages(
+                x["intermediate_steps"]
+            ),
+            "chat_history": lambda x: x["chat_history"],
+        }
+        | prompt
+        | chat_model_with_tools
+        | OpenAIFunctionsAgentOutputParser()
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    return agent_executor
---
+++ src/test.py
@@ -0,0 +1,5 @@
+from agents.agent_env import env_agent
+
+env_agent = env_agent()
+
+env_agent.invoke({"input": "应该允许国外的废旧电池进口吗？", "chat_history": []})
